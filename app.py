ddab85eba8a11aa987ab521b57155c237058c940
VS-zer0 <78421895+VS-zer0@users.noreply.github.com>
2023-12-22 20:29:47 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,49 +1,19 @@
-from flask import Flask, render_template, redirect, request, url_for
-from werkzeug.security import check_password_hash, generate_password_hash, secrets
+from flask import Flask, render_template
 from flask_admin import Admin
-from flask_login import login_user, LoginManager
-from models import db, User, Work, Attachments, Subject, Course
-from views import WorkAdmin, SubjectAdmin, CourseAdmin, UserAuth, AdminIndex
-import os
+from models import db, Work, Attachments, Subject, Course
+from views import WorkAdmin, SubjectAdmin, CourseAdmin
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secrets.token_urlsafe()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databaZe.db'
 db.init_app(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
 
-@login_manager.user_loader
-def load_user(user):
-    u = User.query.get(user)
-    return UserAuth(u.id)
-
-admin = Admin(app, name='Admin Panel', index_view=AdminIndex())
-
-admin.add_view(WorkAdmin(Work, db.session))
-admin.add_view(SubjectAdmin(Subject, db.session))
-admin.add_view(CourseAdmin(Course, db.session))
 
 @app.route('/')
 def home():
     subjects = Subject.query.all()
     return render_template('base.html', subjects=subjects)
 
-@app.route('/login/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        # Check the login credentials
-        if check_password_hash(User.query.first().password, request.form['password']):
-            # Save the login status in the session
-            login_user(load_user(1))
-            return redirect(url_for('admin.index'))
-    return '''
-        <form method="post">
-            <p><input type=password name=password>
-            <p><input type=submit value=Login>
-        </form>
-    '''
 
 @app.route('/<int:subj_id>/')
 def works(subj_id):
@@ -52,6 +22,7 @@ def works(subj_id):
     subjects = Subject.query.all()
     return render_template('works/index.html', courses=courses, subjects=subjects, works=works)
 
+
 @app.route('/<int:subj_id>/<int:crs_id>/')
 def works_course(subj_id, crs_id):
     all_works = Work.query.filter_by(subject_id = subj_id).order_by(Work.date.desc()).all()
@@ -59,6 +30,7 @@ def works_course(subj_id, crs_id):
     courses = list(dict.fromkeys([work.course for work in all_works]))
     subjects = Subject.query.all()
     return render_template('works/index.html', courses=courses, subjects=subjects, works=works)
+
 
 @app.route('/<int:subj_id>/<int:crs_id>/<int:work_id>/')
 def work_detail(subj_id, crs_id, work_id):
@@ -70,10 +42,14 @@ def work_detail(subj_id, crs_id, work_id):
 
 
 if __name__ == '__main__':
+    from flask_admin import Admin
+    from views import WorkAdmin, SubjectAdmin, CourseAdmin
+    
+    admin = Admin(app, name='Admin Panel')
+    admin.add_view(WorkAdmin(Work, db.session))
+    admin.add_view(SubjectAdmin(Subject, db.session))
+    admin.add_view(CourseAdmin(Course, db.session))
     with app.app_context():
         db.create_all()
-        # if not User.query.all():
-        #     db.session.add(User(password = generate_password_hash(os.environ.get('Key'))))
-        #     db.session.commit()
-    # app.run(host='0.0.0.0', debug=True)
+        
     app.run()
--- models.py
+++ models.py
@@ -1,38 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
 db = SQLAlchemy()
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.Text, nullable=False)
-    
-    
-class Work(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    content = db.Column(db.Text, nullable=True)
-    link = db.Column(db.Text, nullable=False)
-    type = db.Column(db.Enum('Link', 'Embed Document', 'Embed Repl.it'), nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
-    
-    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
-    subject = db.relationship('Subject')
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
-    course = db.relationship('Course')
-    
-    attachments = db.relationship('Attachments', cascade="all,delete", backref='work')
-    
-    def __repr__(self):
-        return self.title
-
-
-class Attachments(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.Text, nullable=False)
-    name = db.Column(db.String(255), nullable=False)
-    work_id = db.Column(db.Integer, db.ForeignKey('work.id'), nullable=False)
-    
     
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -40,11 +8,29 @@ class Subject(db.Model):
     def __repr__(self):
         return self.name
     
-    
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     def __repr__(self):
-        return self.name
-    
-    
+        return self.name    
+
+class Work(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text, nullable=True)
+    link = db.Column(db.Text)
+    type = db.Column(db.Enum('No Embed', 'Embed Document', 'Embed Repl.it'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    subject = db.relationship('Subject')
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    course = db.relationship('Course')
+    attachments = db.relationship('Attachments', cascade="all,delete", backref='work')
+    def __repr__(self):
+        return self.title
+
+class Attachments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    link = db.Column(db.Text, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    work_id = db.Column(db.Integer, db.ForeignKey('work.id'), nullable=False)
